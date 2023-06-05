4783600b39c01ac1dabd21e6f13402545cdbd2b5
Takuto <htakutolife@gmail.com>
2023-06-05 23:58:08 +0900

    完了画面作成

---
+++ complate.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>完了画面</title>
+</head>
+<body>
+<h1>完了画面</h1>
+<h2>送信完了</h2>
+<form action="input.php">
+  <input type="submit" value="入力画面へ">
+</form>
+</body>
+</html>
